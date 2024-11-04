80fc8f3af12298bddec65ecdc385005ce9efe4a2
unknown <dan.harabagiu.dimitrescu@gmail.com>
2024-11-04 14:02:10 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from '../public/logo.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src={logo} alt="Logo" />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
